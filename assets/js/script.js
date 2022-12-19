1ffc86254edd5064dde9febdb718a7d70193d438
Claire <claire.hw.cho@gmail.com>
2022-12-18 23:52:28 -0800

    edit APIs and displayFutureWeather

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const savedCityButtonEl = document.querySelector('saved-city-btn');
 const currentContainerEl = document.querySelector('.current-container');
 const futureContainerEl = document.querySelector('.future-container');
 
-
+const APIKey = '53887a25456014fd75935a098671a048'
 
 const formSubmitHandler = function (event) {
     event.preventDefault();
@@ -15,7 +15,8 @@ const formSubmitHandler = function (event) {
     const city = cityInputEl.value.trim();
 
     if (city) {
-        getWeather(city);
+        getCurrentWeather(city);
+        getFutureWeather(city);
 
         cityInputEl.value = '';
     } else {
@@ -27,13 +28,12 @@ const buttonClickHandler = function (event) {
     const savedCity = event.target.getAttribute('data-city');
 
     if (savedCity) {
-        getWeather(savedCity);
+        getCurrentWeather(savedCity);
     }
 };
 
-const getWeather = function (cityname) {
-    const APIKey = '53887a25456014fd75935a098671a048'
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=' + APIKey;
+const getCurrentWeather = function (cityname) {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&appid=' + APIKey;
 
     fetch(apiUrl)
         .then(function (response) {
@@ -42,6 +42,25 @@ const getWeather = function (cityname) {
                 response.json().then(function (data) {
                     console.log(data);
                     displayCurrentWeather(data);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to OpenWeather');
+        });
+}
+
+const getFutureWeather = function (cityname) {
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=' + APIKey;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
                     displayFutureWeather(data);
                 });
             } else {
@@ -68,25 +87,23 @@ const displayCurrentWeather = function (data) {
     const wind = document.createElement('p');
     const humidity = document.createElement('p');
 
-    const name = data.city["name"];
-    const today = data.list[0]
-    const unformattedDate = today["dt_txt"].substring(0, 10).split('-');
-    const formattedDate = unformattedDate[1] + "/" + unformattedDate[2] + "/" + unformattedDate[0];
-    heading.textContent = name + " (" + formattedDate + ")"
+    const name = data["name"];
+    const today = dayjs().format('M/D/YYYY');
+    heading.textContent = name + " (" + today + ")";
 
-    const iconCode = today["weather"][0]["icon"];
+    const iconCode = data["weather"][0]["icon"];
     const iconUrl = 'http://openweathermap.org/img/wn/' + iconCode + '.png';
-    const iconDescription = today["weather"][0]["description"];
+    const iconDescription = data["weather"][0]["description"];
     weatherIcon.setAttribute('src', iconUrl);
     weatherIcon.setAttribute('alt', iconDescription);
     heading.appendChild(weatherIcon);
 
-    temp.textContent = "Temp: " + today["main"]["temp"] + "ºF";
-    wind.textContent = "Wind: " + today["wind"]["speed"] + " MPH";
-    humidity.textContent = "Humidity: " + today["main"]["humidity"] + " %";
+    temp.textContent = "Temp: " + data["main"]["temp"] + "ºF";
+    wind.textContent = "Wind: " + data["wind"]["speed"] + " MPH";
+    humidity.textContent = "Humidity: " + data["main"]["humidity"] + " %";
 
     currentContainerEl.append(heading, temp, wind, humidity);
-}
+};
 
 const displayFutureWeather = function (data) {
     if (data.length === 0) {
@@ -97,34 +114,41 @@ const displayFutureWeather = function (data) {
     // Clear the innerHTML of future weather container.
     futureContainerEl.innerHTML = '';
 
-    const date = document.createElement('p');
-    date.classList = 'future-date';
-    const weatherIcon = document.createElement('img');
-    const temp = document.createElement('p');
-    const wind = document.createElement('p');
-    const humidity = document.createElement('p');
-    const card = document.createElement('div');
-    card.classList = 'card';
+    for (let i = 0; i < data.list.length; i++) {
+        const date = document.createElement('p');
+        date.classList = 'future-date';
+        const weatherIcon = document.createElement('img');
+        weatherIcon.classList = 'weather-icon';
+        const temp = document.createElement('p');
+        const wind = document.createElement('p');
+        const humidity = document.createElement('p');
+        const card = document.createElement('div');
+        card.classList = 'card';
 
-    for (let i = 1; i <= 5; i++) {
         const currentDate = data.list[i];
 
-        const unformattedDate = currentDate["dt_txt"].substring(0, 10).split('-');
-        const formattedDate = unformattedDate[1] + "/" + unformattedDate[2] + "/" + unformattedDate[0];
-        date.textContent = formattedDate
+        if (currentDate["dt_txt"].includes("12:00:00")) {
+            const unformattedDate = currentDate["dt_txt"].substring(0, 10).split('-');
+            const formattedDate = unformattedDate[1] + "/" + unformattedDate[2] + "/" + unformattedDate[0];
+            date.textContent = formattedDate
 
-        const iconCode = currentDate["weather"][0]["icon"];
-        const iconUrl = 'http://openweathermap.org/img/wn/' + iconCode + '.png';
-        const iconDescription = currentDate["weather"][0]["description"];
-        weatherIcon.setAttribute('src', iconUrl);
-        weatherIcon.setAttribute('alt', iconDescription);
+            const iconCode = currentDate["weather"][0]["icon"];
+            const iconUrl = 'http://openweathermap.org/img/wn/' + iconCode + '.png';
+            const iconDescription = currentDate["weather"][0]["description"];
+            weatherIcon.setAttribute('src', iconUrl);
+            weatherIcon.setAttribute('alt', iconDescription);
 
-        temp.textContent = "Temp: " + currentDate["main"]["temp"] + "ºF";
-        wind.textContent = "Wind: " + currentDate["wind"]["speed"] + " MPH";
-        humidity.textContent = "Humidity: " + currentDate["main"]["humidity"] + " %";
+            temp.textContent = "Temp: " + currentDate["main"]["temp"] + "ºF";
+            wind.textContent = "Wind: " + currentDate["wind"]["speed"] + " MPH";
+            humidity.textContent = "Humidity: " + currentDate["main"]["humidity"] + " %";
 
-        card.append(date, weatherIcon, temp, wind, humidity);
-        futureContainerEl.appendChild(card);
+            card.append(date, weatherIcon, temp, wind, humidity);
+            futureContainerEl.append(card);
+        }
+
+        if (document.getElementsByClassName('card').length === 5) {
+            return;
+        }
     }
 }
 
