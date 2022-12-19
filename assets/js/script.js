fca52aba2c0091b1938495434cb501eb228509e4
Claire <claire.hw.cho@gmail.com>
2022-12-18 22:43:03 -0800

    edit apiKey

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ const savedCityButtonEl = document.querySelector('saved-city-btn');
 const currentContainerEl = document.querySelector('.current-container');
 const futureContainerEl = document.querySelector('.future-container');
 
+
+
 const formSubmitHandler = function (event) {
     event.preventDefault();
 
@@ -30,7 +32,8 @@ const buttonClickHandler = function (event) {
 };
 
 const getWeather = function (cityname) {
-    let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=022b16ff817e6ce8bad0d6967ff115f1';
+    const APIKey = '53887a25456014fd75935a098671a048'
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=' + APIKey;
 
     fetch(apiUrl)
         .then(function (response) {
@@ -39,6 +42,7 @@ const getWeather = function (cityname) {
                 response.json().then(function (data) {
                     console.log(data);
                     displayCurrentWeather(data);
+                    displayFutureWeather(data);
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -54,31 +58,29 @@ const displayCurrentWeather = function (data) {
         currentContainerEl.textContent = 'No forecast data found.';
         return;
     }
+
     // Clear the innerHTML of current weather container.
     currentContainerEl.innerHTML = '';
 
-
-    // Create elements and append to the container.
     const heading = document.createElement('h2');
-    const weatherIcon = document.createElement('img');s
+    const weatherIcon = document.createElement('img');
     const temp = document.createElement('p');
     const wind = document.createElement('p');
     const humidity = document.createElement('p');
 
     const name = data.city["name"];
-
     const today = data.list[0]
     const unformattedDate = today["dt_txt"].substring(0, 10).split('-');
-    const currentDate = unformattedDate[1] + "/" + unformattedDate[2] + "/" + unformattedDate[0];
+    const formattedDate = unformattedDate[1] + "/" + unformattedDate[2] + "/" + unformattedDate[0];
+    heading.textContent = name + " (" + formattedDate + ")"
 
     const iconCode = today["weather"][0]["icon"];
     const iconUrl = 'http://openweathermap.org/img/wn/' + iconCode + '.png';
     const iconDescription = today["weather"][0]["description"];
     weatherIcon.setAttribute('src', iconUrl);
     weatherIcon.setAttribute('alt', iconDescription);
-
-    heading.textContent = name + " (" + currentDate + ")"
     heading.appendChild(weatherIcon);
+
     temp.textContent = "Temp: " + today["main"]["temp"] + "ºF";
     wind.textContent = "Wind: " + today["wind"]["speed"] + " MPH";
     humidity.textContent = "Humidity: " + today["main"]["humidity"] + " %";
@@ -88,39 +90,42 @@ const displayCurrentWeather = function (data) {
 
 const displayFutureWeather = function (data) {
     if (data.length === 0) {
-        currentContainerEl.textContent = 'No forecast data found.';
+        futureContainerEl.appendChild('p', 'No forecast data found.');
         return;
     }
-    // Clear the innerHTML of current weather container.
-    currentContainerEl.innerHTML = '';
 
+    // Clear the innerHTML of future weather container.
+    futureContainerEl.innerHTML = '';
 
-    // Create elements and append to the container.
-    const heading = document.createElement('h2');
+    const date = document.createElement('p');
+    date.classList = 'future-date';
     const weatherIcon = document.createElement('img');
     const temp = document.createElement('p');
     const wind = document.createElement('p');
     const humidity = document.createElement('p');
+    const card = document.createElement('div');
+    card.classList = 'card';
 
-    const name = data.city["name"];
+    for (let i = 1; i <= 5; i++) {
+        const currentDate = data.list[i];
 
-    const today = data.list[0]
-    const unformattedDate = today["dt_txt"].substring(0, 10).split('-');
-    const currentDate = unformattedDate[1] + "/" + unformattedDate[2] + "/" + unformattedDate[0];
+        const unformattedDate = currentDate["dt_txt"].substring(0, 10).split('-');
+        const formattedDate = unformattedDate[1] + "/" + unformattedDate[2] + "/" + unformattedDate[0];
+        date.textContent = formattedDate
 
-    const iconCode = today["weather"][0]["icon"];
-    const iconUrl = 'http://openweathermap.org/img/wn/' + iconCode + '.png';
-    const iconDescription = today["weather"][0]["description"];
-    weatherIcon.setAttribute('src', iconUrl);
-    weatherIcon.setAttribute('alt', iconDescription);
+        const iconCode = currentDate["weather"][0]["icon"];
+        const iconUrl = 'http://openweathermap.org/img/wn/' + iconCode + '.png';
+        const iconDescription = currentDate["weather"][0]["description"];
+        weatherIcon.setAttribute('src', iconUrl);
+        weatherIcon.setAttribute('alt', iconDescription);
 
-    heading.textContent = name + " (" + currentDate + ")"
-    heading.appendChild(weatherIcon);
-    temp.textContent = "Temp: " + today["main"]["temp"] + "ºF";
-    wind.textContent = "Wind: " + today["wind"]["speed"] + " MPH";
-    humidity.textContent = "Humidity: " + today["main"]["humidity"] + " %";
+        temp.textContent = "Temp: " + currentDate["main"]["temp"] + "ºF";
+        wind.textContent = "Wind: " + currentDate["wind"]["speed"] + " MPH";
+        humidity.textContent = "Humidity: " + currentDate["main"]["humidity"] + " %";
 
-    currentContainerEl.append(heading, temp, wind, humidity);
+        card.append(date, weatherIcon, temp, wind, humidity);
+        futureContainerEl.appendChild(card);
+    }
 }
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
